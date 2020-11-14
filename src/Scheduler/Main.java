4d9fa9ec322f58e64ce56d3acdcd364144c6fe5f
Logan Andersen <Logan.Andersen@journeyteam.com>
2020-11-14 14:54:01 -0700

    added resource bundles

--- src/Scheduler/Main.java
+++ src/Scheduler/Main.java
@@ -2,6 +2,7 @@ package Scheduler;
 
 import Scheduler.DAO.DBService;
 import Scheduler.DAO.DBConnector;
+import Scheduler.controller.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,18 +11,34 @@ import javafx.stage.Stage;
 
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
-        primaryStage.setTitle("Scheduler");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
+        Locale defaultLocale = Locale.getDefault();
+        ResourceBundle rb = ResourceBundle.getBundle("Scheduler/Nat",defaultLocale);
 
+        FXMLLoader loginLoader = new FXMLLoader();
+        loginLoader.setLocation(getClass().getResource("view/Login.fxml"));
+        loginLoader.setResources(rb);
+        Parent loginParent = loginLoader.load();
+        LoginController loginController = loginLoader.getController();
+
+        Connection conn = DBConnector.connectToDB();
+        loginController.setConn(conn);
+
+        loginController.setLocale(defaultLocale);
+
+        primaryStage.setTitle(rb.getString("primaryStage.title"));
+        primaryStage.setScene(new Scene(loginParent));
+        primaryStage.show();
+
+    }
     public static void main(String[] args) throws SQLException {
         launch(args);
+        DBConnector.closeConnection();
     }
 }
--- src/Scheduler/controller/LoginController.java
+++ src/Scheduler/controller/LoginController.java
@@ -10,6 +10,8 @@ import javafx.event.ActionEvent;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.ZoneId;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable{
@@ -20,8 +22,11 @@ public class LoginController implements Initializable{
     private TextField Password;
     @FXML
     private Label ErrorText;
+    @FXML
+    private Label Zone;
 
     private Connection conn;
+    private Locale locale;
 
     @FXML
     private void login(ActionEvent event) throws SQLException {
@@ -34,12 +39,20 @@ public class LoginController implements Initializable{
         }
         else{
             ErrorText.setVisible(true);
-            ErrorText.setText("Username or Password is incorrect");
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Zone.setText(ZoneId.systemDefault().toString());
         conn = DBConnector.connectToDB();
+    }
+
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
     }
 }
